bfb9485ca1f69ed0059d194fb8efe57ef1d9714e
Prakhar Srivastav <prakhar1989@gmail.com>
2013-12-28 21:12:47 +0300

    Updated readme

---
+++ README.md
@@ -0,0 +1,31 @@
+GettUp
+======
+
+GettUp is a simple command line utility which lets you share and upload files to the [ge.tt](http://ge.tt) sharing service quickly and easily.
+
+## Installation 
+GettUp is distributed as a python package. Do the following to install
+
+``` sh
+sudo pip install gettup
+OR 
+sudo easy_install gettup
+OR
+# download source and cd to it
+sudo python setup.py install
+```
+
+## Usage
+1. Make an account on [Ge.tt](http://ge.tt)
+2. Obtain an api key by [creating an app](http://ge.tt/developers/create).
+3. Run the application. The app will ask for your credentials the first the app is run. 
+
+```sh 
+$ gett
+Please enter your Ge.tt email: example@example.com
+Please enter your Ge.tt password: verysecurepassword
+Please enter your API KEY: supersecretapikey
+```
+
+## License
+Gettup is released under the MIT license.
--- setup.py
+++ setup.py
@@ -1,23 +1,26 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-from distribute_setup import use_setuptools
-use_setuptools()
-
+import sys
 from setuptools import setup, find_packages
 
-setup(name='gett',
+requirements = ['requests']
+if sys.version_info < (2, 7):
+    requirements.append('argparse')
+
+setup(name='gettup',
       version='0.1',
       description='A command-line file sharing utility for ge.tt',
+      keywords="CLI filesharing file sharing upload command-line"
       author='Prakhar Srivastav',
       author_email='prakhar1989@gmail.com',
       url='https://github.com/prakhar1989/gett/',
-      py_modules=['gett'],
-      install_requires=['distribute'],
+      license='MIT',
+      packages=find_packages(),
+      #py_modules=['gett'],
+      install_requires=['requests'],
       entry_points = {
-          'console_scripts': [
-              'gett = gett_uploader:entry_point'
-          ]
-      }
+          'console_scripts': [ 'gett = gett:main' ]
+      },
+      zip_safe=False
      )
 
